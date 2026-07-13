f29f138faae51cd7de2aa02f59990db766c1c8c8
Juan Esteban Rodríguez Céspedes <juan.rodriguez-ce@mail.escuelaing.edu.co>
2026-07-13 15:39:24 -0500

    Mas arreglos de socket

--- src/main/java/com/arsw/bomberman/BombermanApplication.java
+++ src/main/java/com/arsw/bomberman/BombermanApplication.java
@@ -2,19 +2,18 @@ package com.arsw.bomberman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 public class BombermanApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication app = new SpringApplication(BombermanApplication.class);
-		String socket = System.getenv("SOCKETIO_ENABLED");
-		if ("true".equalsIgnoreCase(socket)){
-			app.setWebApplicationType(WebApplicationType.REACTIVE);
+		String socketEnabled = System.getenv("SOCKETIO_ENABLED");
+		if ("true".equalsIgnoreCase(socketEnabled)) {
+			String port = System.getenv("PORT");
+			int tomcatPort = 9999;
+			System.setProperty("server.port", String.valueOf(tomcatPort));
 		}
-		app.run(args);
+		SpringApplication.run(BombermanApplication.class, args);
 	}
 
 }
