f5247b5841d6213c10a1a27c1a7170e8044c1b47
Juan Esteban Rodríguez Céspedes <juan.rodriguez-ce@mail.escuelaing.edu.co>
2026-07-13 14:48:47 -0500

    Arreglos para servicios

--- src/main/java/com/arsw/bomberman/BombermanApplication.java
+++ src/main/java/com/arsw/bomberman/BombermanApplication.java
@@ -2,13 +2,19 @@ package com.arsw.bomberman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 public class BombermanApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BombermanApplication.class, args);
+		SpringApplication app = new SpringApplication(BombermanApplication.class);
+		String socket = System.getenv("SOCKETIO_ENABLED");
+		if ("true".equalsIgnoreCase(socket)){
+			app.setWebApplicationType(WebApplicationType.NONE);
+		}
+		app.run(args);
 	}
 
 }
